307d33418febc42eb3973b28bfeb1a493026fa15
Connor J. Toth <tothconnor@gmail.com>
2022-02-11 19:40:30 -0600

    add database connection

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const mysql = require('mysql');
+const config = require('config');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const mysqlCxnConfig = config.get('mysqlCxnConfig');
+const mysqlCxn = mysql.createConnection({
+  host: mysqlCxnConfig.host,
+  user: mysqlCxnConfig.user,
+  port: mysqlCxnConfig.port,
+  password: mysqlCxnConfig.password,
+  ssl  : {
+    ca : fs.readFileSync(__dirname + '/config/mysql-ca.crt')
+  }
+});
+
+mysqlCxn.connect((err) => {
+  if (err) {
+    console.error("Problem connecting to database...");
+    throw err;
+  }
+
+  console.log("Successfully connected to database");
+});
+
+
 
 app.post('/api/requestVoters', (req, res) => {
   res.send('successfully called api');
