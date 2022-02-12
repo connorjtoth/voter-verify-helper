be90937e34a7712d34590b8ec9b8c0f675225d29
Connor J. Toth <tothconnor@gmail.com>
2022-02-11 21:36:49 -0600

    add query logic to get relevant voters at differing levels of specificity

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// TODO: Modularize code
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -14,11 +16,172 @@ const mysqlCxn = mysql.createConnection({
   user: mysqlCxnConfig.user,
   port: mysqlCxnConfig.port,
   password: mysqlCxnConfig.password,
+  database : mysqlCxnConfig.database,
   ssl  : {
     ca : fs.readFileSync(__dirname + '/config/mysql-ca.crt')
   }
 });
 
+const EXACT_QUERY = ''
++ ' SELECT * FROM Voter'
++ ' WHERE firstName = ?'
++ ' AND lastName = ?'
++ ' AND houseNumber = ?'
++ ' AND preDirection = ?'
++ ' AND streetName = ?'
++ ' AND streetType LIKE ?'
+
+const GOOD_QUERY = ''
++ ' SELECT * FROM Voter'
++ ' WHERE (firstName LIKE ? AND lastName LIKE ?)'
++ ' OR (firstName LIKE ? AND houseNumber = ?)'
++ ' OR (lastName LIKE ? AND houseNumber = ?)'
++ ' OR (houseNumber = ? AND streetName LIKE ?)'
+
+const LAST_CHANCE_QUERY = ''
++ ' SELECT * FROM Voter'
++ ' WHERE (firstName LIKE ?)'
++ ' OR (houseNumber LIKE ?)';
+
+
+class VoterDto {
+
+  constructor(obj) {
+    this.voterid = obj.voterid;
+    this.county = obj.county;
+    this.firstName = obj.firstName;
+    this.middleName = obj.middleName;
+    this.lastName = obj.lastName;
+    this.suffix = obj.suffix;
+    this.houseNumber = obj.houseNumber;
+    this.houseSuffix = obj.houseSuffix;
+    this.preDirection = obj.preDirection;
+    this.streetName = obj.streetName;
+    this.streetType = obj.streetType;
+    this.postDirection = obj.postDirection;
+    this.unitType = obj.unitType;
+    this.unitNumber = obj.unitNumber;
+    this.nonStandardAddress = obj.nonStandardAddress;
+    this.residentialCity = obj.residentialCity;
+    this.residentialState = obj.residentialState;
+    this.residentialZipCode = obj.residentialZipCode;
+  }
+
+  static fromDataStr(datastr) {
+    const data = datastr.split('\t');
+    if (data.length != 6) {
+      throw new Exception('Must have 6 fields of data to construct Voter from data string: ' + datastr);
+    }
+
+    return new VoterDto({
+      firstName: data[0],
+      lastName: data[1],
+      houseNumber: data[2],
+      preDirection: data[3],
+      streetName: data[4],
+      streetType: data[5]
+    });
+  }
+}
+
+
+function sqlQueryPromise(...args) {
+  return new Promise((resolve, reject) => {
+    mysqlCxn.query(...args, (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(results);
+    });
+  });
+}
+
+
+async function queryMatch(voterDto, type, query, params) {
+  console.log(`Checking for a ${type} match...`);
+  try {
+    let results = await sqlQueryPromise(query, params);
+
+    let voterDtoList = [];
+
+    if (results.length > 0) {
+      console.log(`A ${type} match was found`);
+      for (let result of results) {
+        voterDtoList.push(new VoterDto(result));
+      }
+    }
+
+    return {
+      type: type,
+      query: voterDto,
+      results: voterDtoList
+    };
+  }
+  catch (error) {
+    console.error(`An error occurred retrieving database results for a ${type} match`);
+    throw error;
+  }
+}
+
+
+async function queryExactMatch(voterDto) {
+  return await queryMatch(voterDto, 'exact', EXACT_QUERY, [
+    `${voterDto.firstName}`,
+    `${voterDto.lastName}`,
+    `${voterDto.houseNumber}`,
+    `${voterDto.preDirection}`,
+    `${voterDto.streetName}`,
+    `${voterDto.streetType}%`
+  ]);
+}
+
+
+async function queryGoodMatch(voterDto) {
+  return await queryMatch(voterDto, 'good', GOOD_QUERY, [
+    `${voterDto.firstName}%`, `${voterDto.lastName}%`,
+    `${voterDto.firstName}%`, `${voterDto.houseNumber}`,
+    `${voterDto.lastName}%`, `${voterDto.houseNumber}`,
+    `${voterDto.houseNumber}`, `${voterDto.streetName}%`
+  ]);
+}
+
+
+async function queryLastChanceMatch(voterDto) {
+  return await queryMatch(voterDto, 'lastChance', LAST_CHANCE_QUERY, [
+    `%${voterDto.firstName}%`,
+    `${voterDto.houseNumber}%`
+  ]);
+}
+
+
+async function handleRequestVoters(req, res) {
+  console.log(req.body.query);
+
+  const {query, level} = req.body;
+  
+  // First get the data out of the request
+  const voterDto = VoterDto.fromDataStr(req.body.query);
+  console.log("Getting voter data for :");
+  console.log(voterDto);
+
+  // Then make calls to the database to get appropriate results
+  let matchToken;
+  if (!level || level === 'exact') {
+    matchToken = await queryExactMatch(voterDto);
+  }
+  if ((!level && matchToken.results.length === 0) || level === 'good') {
+    matchToken = await queryGoodMatch(voterDto);
+  }
+  if ((!level && matchToken.results.length === 0) || level === 'lastChance') {
+    matchToken = await queryLastChanceMatch(voterDto);
+  }
+
+  // Return results to the user
+  res.send(matchToken);
+}
+
+
+/* * * MySQL Connection * * */
 mysqlCxn.connect((err) => {
   if (err) {
     console.error("Problem connecting to database...");
@@ -30,12 +193,8 @@ mysqlCxn.connect((err) => {
 
 
 
-app.post('/api/requestVoters', (req, res) => {
-  res.send('successfully called api');
-  console.log(req.body.query);
-  // TODO: pull data from the request
-  // TODO: add logic to call database and manip data
-});
+/* * * Routing * * */
+app.post('/api/requestVoters', handleRequestVoters);
 
 app.use('/res', express.static(path.join(__dirname, 'res')));
 
